b4341e8892c40233209294888c55a3db1b6631ee
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-16 00:18:51 +0800

    Create skeleton backend code

---
+++ script.js
@@ -0,0 +1,38 @@
+const numbersDiv = document.querySelector(".numbers");
+const operatorsDiv = document.querySelector(".operators");
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, op) {
+    switch (op) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        default:
+            return divide(x, y);
+    }
+}
+
+function calculate() {
+    const firstInput = document.querySelector("#first");
+    const secondInput = document.querySelector("#second");
+    const operator = document.querySelector("#op");
+    return operate(firstInput, secondInput, operator);
+}
